d03df1a7aebca1301e03eea45c8c6e26de896130
Sam O <tortillabarllc@gmail.com>
2020-02-25 05:06:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,52 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_array = ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  array << "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
+  array.unshift ("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays = array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  wow = array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 2
+  array[2]
   
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
   
 end
